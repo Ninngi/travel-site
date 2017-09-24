f8c0df2ff0506c8c27e01cd7b2d02defe3687df3
Patryk <thepatrvq@gmail.com>
2017-09-25 00:40:26 +0200

    gulp change

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,2 @@
-const gulp = require('gulp');
-watch = require("gulp-watch");
-
-gulp.task("default", function() {
-    console.log("Horray -you created a gulp task")
-});
-
-gulp.task("html", function() {
-    console.log("Imagine something usful being done to your HTML here.");
-});
-
-gulp.task("styles", function() {
-    console.log("Imagine Sass or PostCSS task running here.");
-});
-
-gulp.task("watch", function(){
-    watch("./app/index.html", function(){
-        gulp.start("html")
-    });
-
-    watch("./app/assets/styles/**/*.css", function() {
-        gulp.start("styles");
-    })
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
